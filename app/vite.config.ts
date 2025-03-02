89de9184f4c7977fa09d1006406c38fef87547a8
chandima-dev <sanjusooriyaarachchi99@gmail.com>
2025-03-02 21:25:45 +0530

    update  config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,34 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Enable minification for production
-    minify: 'terser', // Use Terser for minification
+    minify: 'terser',
     terserOptions: {
       compress: {
-        drop_console: true, // Remove console logs from production builds
+        drop_console: true,
       },
       output: {
-        comments: false, // Remove comments from output
+        comments: false,
       },
     },
     rollupOptions: {
-      // Additional Rollup options for advanced configurations
       output: {
-        // Custom output configurations (e.g., chunking)
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0]; // Split vendor code into separate chunks
+            return id.split('node_modules/')[1].split('/')[0];
           }
         },
       },
     },
   },
+  server: {
+    historyApiFallback: true, // Ensures Vite dev server works with React Router
+  },
   define: {
-    // Define global constants for your app
-    'process.env': {}, // Helps with environment variables
+    'process.env': {},
   },
 });
